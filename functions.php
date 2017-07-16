f531a38a88540c8f271afa408ddc6af00cf11b07
Ben Joy <ben.joy1@sky.com>
2017-07-16 12:43:43 +0100

    finish updating custom post type to be soundbyte

--- functions.php
+++ functions.php
@@ -345,7 +345,7 @@ add_action('wp_print_scripts', 'html5blank_conditional_scripts'); // Add Conditi
 add_action('get_header', 'enable_threaded_comments'); // Enable Threaded Comments
 add_action('wp_enqueue_scripts', 'html5blank_styles'); // Add Theme Stylesheet
 add_action('init', 'register_html5_menu'); // Add HTML5 Blank Menu
-add_action('init', 'create_post_type_html5'); // Add our Soundbyte Type
+add_action('init', 'create_post_type_soundbyte'); // Add our Soundbyte Type
 add_action('widgets_init', 'my_remove_recent_comments_style'); // Remove inline Recent Comment Styles from wp_head()
 add_action('init', 'html5wp_pagination'); // Add our HTML5 Pagination
 
@@ -396,25 +396,25 @@ add_shortcode('html5_shortcode_demo_2', 'html5_shortcode_demo_2'); // Place [htm
 \*------------------------------------*/
 
 // Create 1 Custom Post type for a Demo, called HTML5-Blank
-function create_post_type_html5()
+function create_post_type_soundbyte()
 {
-    register_taxonomy_for_object_type('category', 'html5-blank'); // Register Taxonomies for Category
-    register_taxonomy_for_object_type('post_tag', 'html5-blank');
-    register_post_type('html5-blank', // Register Custom Post Type
+    register_taxonomy_for_object_type('category', 'Soundbyte'); // Register Taxonomies for Category
+    register_taxonomy_for_object_type('post_tag', 'Soundbyte');
+    register_post_type('Soundbyte', // Register Custom Post Type
         array(
         'labels' => array(
-            'name' => __('Soundbyte', 'html5blank'), // Rename these to suit
-            'singular_name' => __('Soundbyte', 'html5blank'),
-            'add_new' => __('Add New', 'html5blank'),
-            'add_new_item' => __('Add New Soundbyte', 'html5blank'),
-            'edit' => __('Edit', 'html5blank'),
-            'edit_item' => __('Edit Soundbyte', 'html5blank'),
-            'new_item' => __('New Soundbyte', 'html5blank'),
-            'view' => __('View Soundbyte', 'html5blank'),
-            'view_item' => __('View Soundbyte', 'html5blank'),
-            'search_items' => __('Search Soundbyte', 'html5blank'),
-            'not_found' => __('No Soundbytes found', 'html5blank'),
-            'not_found_in_trash' => __('No Soundbytes found in Trash', 'html5blank')
+            'name' => __('Soundbyte', 'Soundbyte'), // Rename these to suit
+            'singular_name' => __('Soundbyte', 'Soundbyte'),
+            'add_new' => __('Add New', 'Soundbyte'),
+            'add_new_item' => __('Add New Soundbyte', 'Soundbyte'),
+            'edit' => __('Edit', 'Soundbyte'),
+            'edit_item' => __('Edit Soundbyte', 'Soundbyte'),
+            'new_item' => __('New Soundbyte', 'Soundbyte'),
+            'view' => __('View Soundbyte', 'Soundbyte'),
+            'view_item' => __('View Soundbyte', 'Soundbyte'),
+            'search_items' => __('Search Soundbyte', 'Soundbyte'),
+            'not_found' => __('No Soundbytes found', 'Soundbyte'),
+            'not_found_in_trash' => __('No Soundbytes found in Trash', 'Soundbyte')
         ),
         'public' => true,
         'hierarchical' => true, // Allows your posts to behave like Hierarchy Pages
