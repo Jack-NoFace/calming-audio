7df86d4d2100ac2a0e8e09476f229b1e1e13e39e
Jack Davies <jackdavieswork@hotmail.co.uk>
2017-07-16 13:44:01 +0100

    Remove jQuery

--- functions.php
+++ functions.php
@@ -515,4 +515,13 @@ function fix_svg() {
 }
 add_action( 'admin_head', 'fix_svg' );
 
+// Remove jQuery Migrate Script from header and Load jQuery from Google API
+function crunchify_stop_loading_wp_embed_and_jquery() {
+	if (!is_admin()) {
+		wp_deregister_script('wp-embed');
+		wp_deregister_script('jquery');  // Bonus: remove jquery too if it's not required
+	}
+}
+add_action('init', 'crunchify_stop_loading_wp_embed_and_jquery');
+
 ?>
