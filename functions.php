c3ab09f44c5f78116024f6883357b2130dfdcd8a
Jack Davies <jackdavieswork@hotmail.co.uk>
2017-07-16 13:44:13 +0100

    Remove WP Emoji

--- functions.php
+++ functions.php
@@ -524,4 +524,10 @@ function crunchify_stop_loading_wp_embed_and_jquery() {
 }
 add_action('init', 'crunchify_stop_loading_wp_embed_and_jquery');
 
+// Remove WP Emoji
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_print_styles', 'print_emoji_styles');
+remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+remove_action( 'admin_print_styles', 'print_emoji_styles' );
+
 ?>
