701e6f0a2bc71e9922ec361be4b49d172c5f5639
Ben Joy <ben.joy1@sky.com>
2017-07-18 14:39:19 +0100

    add audio style to loop on audio page

--- page-calming.php
+++ page-calming.php
@@ -3,36 +3,44 @@
 	<main role="main">
 		<!-- section -->
 		<section>
+			<div class="audio-wrapper">
 
-			<h1><?php the_title(); ?></h1>
+			
+			
+				<?php
+				$args = array (
+					'post_type'         => 'Soundbyte',
+					'category_name'				=> 'calming'
+				);
 
-		<?php
-        $args = array (
-            'post_type'         => 'Soundbyte',
-			'category_name'				=> 'calming'
-        );
+				$the_query = new WP_Query ($args);
 
-        $the_query = new WP_Query ($args);
+				if ($the_query->have_posts() ) : while ($the_query->have_posts() ) : $the_query->the_post(); ?>
+				
+					
+				<div class="soundbyte">	 
+					
+					<?php the_post_thumbnail('thumbnail', array ('class' => 'audio-icon')); ?>
+					<?php the_post_thumbnail('thumbnail', array ('class' => 'audio-icon')); ?>
+					<?php the_post_thumbnail('thumbnail', array ('class' => 'audio-icon')); ?>
+					<p><?php the_title(); ?></p></br>
+					<!--<audio controls loop> 
+						<source src="<?php the_field('media'); ?>" type="audio/mp3"> 
+						Your browser does not support the audio element. 
+					</audio> -->
+					
+				</div>
 
-         if ($the_query->have_posts() ) : while ($the_query->have_posts() ) : $the_query->the_post(); ?>
-          
-            <div> 
-				<?php the_post_thumbnail(); ?>
-                <?php the_title(); ?></br>
-				<audio controls loop> 
-					<source src="<?php the_field('media'); ?>" type="audio/mp3"> 
-					Your browser does not support the audio element. 
-				</audio>
-         	</div>
-         
-        <?php endwhile; else : ?>
-            <p><?php echo ("No sounds to display");?></p>
-        <?php endif; ?>
+					<?php endwhile; else : ?>
+						<p><?php echo ("No sounds to display");?></p>
+					<?php endif; ?>
+				
+				
 
-		<?php wp_reset_postdata(); ?>
+					<?php wp_reset_postdata(); ?>
+				
 
-
-
+			</div>
 		</section>
 		<!-- /section -->
 	</main>
