a567b72073c152be8d9935aef8d9418204ad82c7
JackDaviesWork <jackdavieswork@hotmail.co.uk>
2017-08-11 13:23:21 +0100

    Add favicon

--- header.php
+++ header.php
@@ -5,8 +5,12 @@
 		<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
 
 		<link href="//www.google-analytics.com" rel="dns-prefetch">
-        <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
-        <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
+		<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/img/icons/apple-touch-icon.png">
+		<link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon-32x32.png">
+		<link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon-16x16.png">
+		<link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/img/icons/manifest.json">
+		<link rel="mask-icon" href="<?php echo get_template_directory_uri(); ?>/img/icons/safari-pinned-tab.svg" color="#5bbad5">
+		<meta name="theme-color" content="#8800ff">
 
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -24,7 +28,7 @@
 			<header class="header clear" role="banner">
 
 				<div class="top-bar">
-				
+
 					<div class="breakpoint-nav">
 						<!-- nav -->
 						<nav class="nav" role="navigation">
@@ -33,7 +37,7 @@
 						</nav>
 					</div>
 						<!-- /nav -->
-				
+
 				<!-- play controls -->
 				<div class="breakpoint-play-controls">
 					<div class="play-controls">
@@ -44,11 +48,11 @@
 					</div>
 				</div>
 				<!-- play controls -->
-				
 
-			
 
-			
+
+
+
 
 			</header>
 			<!-- /header -->
