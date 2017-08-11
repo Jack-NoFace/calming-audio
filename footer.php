c9e7e0010d0b81994ab6a5daa40791e10e4ae1a5
JackDaviesWork <jackdavieswork@hotmail.co.uk>
2017-08-11 13:25:04 +0100

    Add analytics

--- footer.php
+++ footer.php
@@ -1,14 +1,14 @@
 			<!-- footer -->
 			<footer class="footer" role="contentinfo">
 				<p>Designed by <a href="https://whatjackhasmade.co.uk">Jack Davies</a> & Developed by <a href="https://benmichaeljoy.com">Ben Joy</a></a></p>
-				
-				
+
+
 				<div class="bottom-line">
 					<div class="social-media">
 						<a href="https://twitter.com/calmingaudio"><div class="social"><svg class="icon social-icon"><use xlink:href="<?php echo get_template_directory_uri(); ?>/img/icons/sprite-sheet.svg#twitter"/></svg></div></a>
 						<a href="https://www.facebook.com/Calming-Audio-824246891033884/"><div class="social"><svg class="icon social-icon"><use xlink:href="<?php echo get_template_directory_uri(); ?>/img/icons/sprite-sheet.svg#facebook"/></svg></div></a>
 						<a href="https://github.com/JackDaviesWork/calming-audio"><div class="social"><svg class="icon social-icon"><use xlink:href="<?php echo get_template_directory_uri(); ?>/img/icons/sprite-sheet.svg#github"/></svg></div></a>
-						
+
 					</div>
 
 					<div class="copyright">
@@ -28,12 +28,12 @@
 
 		<!-- analytics -->
 		<script>
-		(function(f,i,r,e,s,h,l){i['GoogleAnalyticsObject']=s;f[s]=f[s]||function(){
-		(f[s].q=f[s].q||[]).push(arguments)},f[s].l=1*new Date();h=i.createElement(r),
-		l=i.getElementsByTagName(r)[0];h.async=1;h.src=e;l.parentNode.insertBefore(h,l)
-		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-		ga('create', 'UA-XXXXXXXX-XX', 'yourdomain.com');
-		ga('send', 'pageview');
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+			ga('create', 'UA-102667525-1', 'auto');
+			ga('send', 'pageview');
 		</script>
 
 	</body>
