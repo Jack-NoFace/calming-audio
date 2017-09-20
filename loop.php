d5b23782b94e277aedeab6a1b54d7b2c973ddda2
JackDaviesWork <jackdavieswork@hotmail.co.uk>
2017-09-20 12:19:43 +0100

    Add play/pause one at a time to individual sounds

--- loop.php
+++ loop.php
@@ -1,23 +1,25 @@
 
 <div class="audio-wrapper">
 <?php if (have_posts()): while (have_posts()) : the_post(); ?>
-	
+
 	<!-- article -->
-	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>> 
-	
-		<div class="soundbyte">	 
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+		<div class="soundbyte">
 			<div class="soundbyte-icons">
 				<?php the_post_thumbnail('thumbnail', array ('class' => 'audio-icon')); ?>
-				
+
 				<audio loop id="audio_player_<?php echo $id;?>">
 					<source src="<?php the_field('media'); ?>" type="audio/mp3">
 					Your browser does not support the audio element.
 				</audio>
-				
+
 
 				<div>
-					<svg class="icon"><use xlink:href="<?php echo get_template_directory_uri(); ?>/img/icons/sprite-sheet.svg#play-circle"/ onclick="document.getElementById('audio_player_<?php echo $id;?>').play()"></svg> 
-					<svg class="icon"><use xlink:href="<?php echo get_template_directory_uri(); ?>/img/icons/sprite-sheet.svg#pause-circle"/ onclick="document.getElementById('audio_player_<?php echo $id;?>').pause()"></svg> </br>
+					<svg class="icon" id="play-<?php echo $id;?>"><use xlink:href="<?php echo get_template_directory_uri(); ?>/img/icons/sprite-sheet.svg#play-circle"/ onclick="play()"></svg>
+
+					<svg class="icon display--none" id="pause-<?php echo $id;?>"><use xlink:href="<?php echo get_template_directory_uri(); ?>/img/icons/sprite-sheet.svg#pause-circle"/ onclick="pause()"></svg> </br>
+
 					<svg class="icon"><use xlink:href="<?php echo get_template_directory_uri(); ?>/img/icons/sprite-sheet.svg#volume-2"/ onclick="document.getElementById('audio_player_<?php echo $id;?>').volume += 0.1"></svg>
 					<svg class="icon"><use xlink:href="<?php echo get_template_directory_uri(); ?>/img/icons/sprite-sheet.svg#volume-x"/ onclick="document.getElementById('audio_player_<?php echo $id;?>').volume -= 0.1"></svg>
 					<input id="vol-control" type="range" min="0" max="100" step="1" oninput="SetVolume(this.value)" onchange="SetVolume(this.value)"></input>
@@ -29,16 +31,28 @@
 		<script>
 			window.SetVolume = function(val)
 			{
-			var player = document.getElementById('audio_player_<?php echo $id; ?>');
-			console.log('Before: ' + player.volume);
-			player.volume = val / 100;
-			console.log('After: ' + player.volume);
+				var player = document.getElementById('audio_player_<?php echo $id; ?>');
+				console.log('Before: ' + player.volume);
+				player.volume = val / 100;
+				console.log('After: ' + player.volume);
+			}
+
+			function play() {
+				document.getElementById('audio_player_<?php echo $id;?>').play();
+				document.getElementById("play-<?php echo $id;?>").classList.add("display--none");
+				document.getElementById("pause-<?php echo $id;?>").classList.remove("display--none");
+			}
+
+			function pause() {
+				document.getElementById('audio_player_<?php echo $id;?>').pause();
+				document.getElementById("pause-<?php echo $id;?>").classList.add("display--none");
+				document.getElementById("play-<?php echo $id;?>").classList.remove("display--none");
 			}
 		</script>
-		
+
 	</article>
 	<!-- /article -->
-	
+
 <?php endwhile; ?>
 
 <?php else: ?>
